8b26c4114f6ae8ae64e38dd8ce737d29466477ae
hzlshen <m15277968105@163.com>
2018-01-01 23:38:19 +0800

    ok

--- src/providers/goods-service/goods-service.ts
+++ src/providers/goods-service/goods-service.ts
@@ -1,7 +1,10 @@
-import { Injectable } from '@angular/core';
-import {Http} from "@angular/http";
-import {Observable} from "rxjs/Observable";
-
+import {Injectable} from '@angular/core';
+import {Http,Response } from '@angular/http';
+import 'rxjs/add/operator/map';
+import {GOODLIST_URL} from "./Constants";
+import {Observable} from 'rxjs/Observable'
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';;
 
 @Injectable()
 export class GoodsServiceProvider {
@@ -19,6 +22,19 @@ export class GoodsServiceProvider {
   private extractData(res:Response){
     let body = res.json().data;
     return body || {};
+  }
+
+  private handleError(error: Response | any) {
+    let errMsg: string;
+    if (error instanceof Response) {
+      const body = error.json() || '';
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+    }
+    console.error(errMsg);
+    return Observable.throw(errMsg);
   }
 
 
