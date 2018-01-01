d478274a9a564c89231d2cd2ce4131ffbe6696f0
hzlshen <m15277968105@163.com>
2018-01-01 23:26:05 +0800

    service

--- src/pages/tab/good-list/good-list.ts
+++ src/pages/tab/good-list/good-list.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {GoodsServiceProvider} from "../../../providers/goods-service/goods-service";
+import {GoodDetailPage} from "../good-detail/good-detail";
 
 
 @IonicPage()
--- src/providers/goods-service/goods-service.ts
+++ src/providers/goods-service/goods-service.ts
@@ -1,17 +1,26 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import {Http} from "@angular/http";
+import {Observable} from "rxjs/Observable";
 
-/*
-  Generated class for the GoodsServiceProvider provider.
 
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class GoodsServiceProvider {
 
-  constructor(public http: HttpClient) {
+  constructor(public http: Http) {
     console.log('Hello GoodsServiceProvider Provider');
   }
+
+  public getGoodLists():Observable<string>{
+    return this.http.get(GOODLIST_URL)
+      .map(this.extractData)
+      .catch(this.handleError);
+  }
+
+  private extractData(res:Response){
+    let body = res.json().data;
+    return body || {};
+  }
+
+
 
 }
