fcc082264a7aa42d1b981fa75e8d68cbb2257c2e
hzlshen <m15277968105@163.com>
2018-01-05 00:55:17 +0800

    look ts

--- src/pages/tab/tab-broadcast/tab-broadcast.ts
+++ src/pages/tab/tab-broadcast/tab-broadcast.ts
@@ -1,25 +1,43 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the TabBroadcastPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
-@IonicPage()
+import {Component, ViewChild} from '@angular/core';
+import { NavController, NavParams, Slides} from 'ionic-angular';
+import {LookService} from "../../../providers/look-service";
+import {GOODLIST_head_URL} from "../../../providers/Constants";
 @Component({
-  selector: 'page-tab-broadcast',
-  templateUrl: 'tab-broadcast.html',
+  selector: 'page-tab-broadcast-page',
+  templateUrl: 'tab-look-page.html',
 })
 export class TabBroadcastPage {
+  @ViewChild('mySlider') slider: Slides;
+  tabToShow: Array<boolean> = [true, true, true, true, true, true, true, true, true];
+  scrollableTabsopts: any = {};
+  obj_CategorysListData = [];
+  obj_GoodsListData = [];
+  tabsPlacement: string = "top";
+  errorMessage: string;
+  Image_head_Url: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public lookservice: LookService) {
+    this.Image_head_Url=GOODLIST_head_URL;
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad TabBroadcastPage');
+    this.lookservice.getAllcategory().subscribe(
+      countries => this.obj_CategorysListData = <any>countries,
+      error => this.errorMessage = <any>error,
+      function complete() {
+      },
+    );
+    this.lookservice.getGoodsList().subscribe(
+      countries => this.obj_GoodsListData = <any>countries,
+      error => this.errorMessage = <any>error,
+      function complete() {
+      },
+    );
+  }
+
+
+  selectedFriends(index) {
+    console.log("Segment changed to", index);
   }
 
 }
