82f2537927552bce59c798a5260428291635c1ab
hzlshen <m15277968105@163.com>
2018-01-23 23:36:17 +0800

    00

--- src/providers/HttpUtils.ts
+++ src/providers/HttpUtils.ts
@@ -30,26 +30,6 @@ export class HttpUtils {
     }
     return params;
   }
-  // getGoods(url: string): Observable<string[]> {
-  //   return this.http.get(url)
-  //     .map(this.extractData)
-  //     .catch(this.handleError);
-  // }
-  // private extractData(res: Response) {
-  //   let body = res.json();
-  //   return body || { };
-  // }
-  // private handleError (error: Response | any) {
-  //   let errMsg: string;
-  //   if (error instanceof Response) {
-  //     const body = error.json() || '';
-  //     const err = body.error || JSON.stringify(body);
-  //     errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
-  //   } else {
-  //     errMsg = error.message ? error.message : error.toString();
-  //   }
-  //   console.error(errMsg);
-  //   return Observable.throw(errMsg);
-  // }
+
 
 }
